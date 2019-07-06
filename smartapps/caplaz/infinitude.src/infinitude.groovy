b15c315375a8281cd28d3a11e59e64fc27745f9b
Stefano Acerbetti <acerbetti@gmail.com>
2019-07-05 19:59:32 -0700

    set hold API

--- smartapps/caplaz/infinitude.src/infinitude.groovy
+++ smartapps/caplaz/infinitude.src/infinitude.groovy
@@ -63,6 +63,36 @@ def refresh() {
     }
 }
 
+def refreshChild(dni) {
+	log.debug "Refreshing for child " + dni
+	//def dni = [ app.id, systemID, zone.'@id'.text().toString() ].join('|')
+
+    def d = getChildDevice(dni)
+    if(d) {
+    	d.zUpdate(state.data[dni].temperature,
+            state.data[dni].thermostatOperatingState,
+            state.data[dni].humidity,
+            state.data[dni].heatingSetpoint,
+            state.data[dni].coolingSetpoint,
+            state.data[dni].thermostatFanMode,
+            state.data[dni].thermostatActivityState)
+		log.debug "Data sent to DH"
+        
+        
+                            //    temperature: zone.rt.toInteger(),
+                              //  humidity: zone.rh.toInteger(),
+                              //  coolingSetpoint: zone.clsp.toInteger(),
+                              //  heatingSetpoint: zone.htsp.toInteger(),
+                              //  thermostatFanMode: lookupInfo( "thermostatFanMode", zone.fan.text().toString(), true ),
+                              //  thermostatOperatingState: lookupInfo( "thermostatOperatingState", response.data.mode.text().toString(), true ),
+                               // thermostatActivityState: zone.currentActivity.text().toString(),
+                                
+	}
+    else {
+    	log.debug "Somethig went wrong!"
+    }
+}
+
 
 
 /***********
@@ -90,7 +120,6 @@ private getZonesConfig() {
 }
 
 def getZonesConfigParser(physicalgraph.device.HubResponse hubResponse) {
-
     if (hubResponse.status == 200) {
         def body = hubResponse.json
 
@@ -139,14 +168,70 @@ private getZoneStatus(zoneId) {
 }
 
 def getZoneStatusParser(physicalgraph.device.HubResponse hubResponse) {
-
     if (hubResponse.status == 200) {
-        def body = hubResponse.json
-        
+        def zone = hubResponse.json
+        def dni  = zoneToDni(zone.id)
+
+        log.debug "Refreshing for zone " + dni
+
+        def device = getChildDevice(dni)
+        if (device) {
+            device.updateState(zone.id, zone)
+            
+            log.debug "Data sent to DH"
+
+        } else {
+            log.error "Somethig went wrong!"
+        }
+
     } else {
         log.error "NETWORK ERROR"
     }
 }
+
+
+
+/***********
+ * CHANGE  *
+ ***********/
+
+def setHold(zoneId, activity, hold = 'on', until = '23:45') {
+    def command = "hold=${hold}&activity=${activity}&until=${until}"
+    
+    log.debug "Set hold for zone:${zoneId} with ${command}"
+
+    try {
+        def hubAction = new physicalgraph.device.HubAction(
+            [
+                method: "POST",
+                path: "/api/${zoneId}/hold",
+                body: command,
+                headers: [ 
+                    HOST: getInfinitudeHost(), 
+                    "Content-Type": "application/x-www-form-urlencoded" 
+                ]
+            ],
+            null,
+            [ callback: setHoldParser ]
+        )
+
+        sendHubCommand(hubAction)
+
+        return true
+
+    } catch (Exception e) {
+        log.error "getZonesConfig(): Exception ${e} on ${hubAction}"
+        
+        return false
+    }
+}
+
+def setHoldParser(physicalgraph.device.HubResponse hubResponse) {
+    if (hubResponse.status != 200) {
+        log.error "Error setting the hold: ${hubResponse}"
+    }
+}
+
 
 
 
